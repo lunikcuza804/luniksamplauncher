1e2ea9867468450bd74fdc6425d0325aee8ed36f
lunikcuza804 <98224575+lunikcuza804@users.noreply.github.com>
2023-04-03 18:25:18 +0300

    Create settings.php

---
+++ source_code/src_generated/app/forms/settings.php
@@ -0,0 +1,43 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class settings extends AbstractForm
+{
+
+    /**
+     * @event buttonAlt.click 
+     */
+    function doButtonAltClick(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$this->loadForm('MainForm', false, true);
+
+        
+    }
+
+    /**
+     * @event button.click 
+     */
+    function doButtonClick(UXMouseEvent $e = null)
+    {    
+        fs::makeDir('./settings/');
+        fs::makeFile('./settings/gamefolder.txt');
+        $game = $this->game->text;
+        file_put_contents('./settings/gamefolder.txt', $game);
+        $this->toast('Saved!');
+    }
+
+    /**
+     * @event construct 
+     */
+    function doConstruct(UXEvent $e = null)
+    {    
+        $savedgame = file_get_contents('./settings/gamefolder.txt', true);
+        $this->game->text = $savedgame;
+    }
+
+}
